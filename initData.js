a3b39c998cbf3dfc9d6010bd2fe5110aa9f444ce
Fabio Ferreira <fabiomataferreira@gmail.com>
2022-07-08 10:47:25 -0400

    Cria inicialização de dados

---
+++ initData.js
@@ -0,0 +1,51 @@
+const fetch =
+  (...args) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(...args))
+
+const baseURL = 'http://localhost:3000'
+const PERSON_ENDPOINT = '/person'
+const RELATIONSHIP_ENDPOINT = '/relationship'
+const people = [
+  {
+    cpf: '11111111111',
+    name: 'Fulano da Silva'
+  },
+  {
+    cpf: '22222222222',
+    name: 'Ciclano da Silva'
+  },
+  {
+    cpf: '33333333333',
+    name: 'Beltrano Souza'
+  }
+]
+
+const relationships = [
+  {
+    cpf1: '11111111111',
+    cpf2: '22222222222'
+  },
+  {
+    cpf1: '22222222222',
+    cpf2: '33333333333'
+  }
+]
+
+const options = {
+  headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  method: 'POST'
+}
+
+const promises = people.map(
+  person =>
+    fetch(baseURL + PERSON_ENDPOINT, { ...options, body: JSON.stringify(person) })
+)
+
+Promise.all(promises).then(() => {
+  relationships.forEach(
+    relationship =>
+      fetch(baseURL + RELATIONSHIP_ENDPOINT, {
+        ...options, body: JSON.stringify(relationship)
+      })
+  )
+})
